d5e736e822f15d3abc1b361c4e333b10ebeeeb01
Max Tan Kia Lok <128026743+Kia-Lok@users.noreply.github.com>
2025-12-31 12:00:35 +0800

    Create Part1.java

---
+++ Day 4/Part1.java
@@ -0,0 +1,67 @@
+package Day4;
+
+import java.nio.file.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+import java.lang.Math;
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Challenge1 {
+    public static void main(String[] args) {
+        try {
+            List<String> lines = Files.readAllLines(
+                    Paths.get("/Users/kialok/IdeaProjects/Day 1/src/Day4/input_day4.txt")
+            );
+            List<List<Character>> grid = new ArrayList<>();
+            int len = lines.get(0).length();
+            List<Character> buffer_row = new ArrayList<>();
+            for (int i = 0; i < len + 2; i++) {
+                buffer_row.add('.');
+            }
+            grid.add(buffer_row);
+            for (String line : lines) {
+                List<Character> row = new ArrayList<>();
+                row.add('.');
+                for (char c : line.toCharArray()) {
+                    row.add(c);
+                }
+                row.add('.');
+                grid.add(row);
+            }
+            grid.add(buffer_row);
+
+            int length = grid.size();
+            int acc = 0;
+            for (int i = 1; i < length - 1; i++) {
+                for (int j = 1; j < length - 1; j++) {
+                    if (grid.get(i).get(j) == '@') {
+                        acc += checker(i, j,grid);
+                    }
+                }
+                System.out.println("One row down");
+            }
+            System.out.println(acc);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int checker(int i, int j, List<List<Character>> grid) {
+        int acc = -1;
+        for (int x = i - 1; x <= i + 1; x++) {
+            for (int y = j - 1; y <= j + 1; y++) {
+                if (grid.get(x).get(y) == '@') {
+                    acc++;
+                }
+            }
+        }
+        if (acc < 4) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
